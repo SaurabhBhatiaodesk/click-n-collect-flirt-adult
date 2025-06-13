f371e3a1dc981d22c87c4ef843a59f294ff6b5a3
Rahul <rahulherb1995@gmail.com>
2025-06-13 16:12:34 +0530

    auth

--- app/routes/api.pickupLocation/route.jsx
+++ app/routes/api.pickupLocation/route.jsx
@@ -9,7 +9,7 @@ export async function loader({ request }) {
   const auth_session = await db.session.findFirst({
     where: { shop },
   });
-
+  console.log('auth_session', auth_session);
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("X-Shopify-Access-Token", auth_session?.accessToken);
